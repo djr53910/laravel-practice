0879f0493bd6f89915a805f65c5d228f9ea9bb83
djr53910 <djr0418@gmail.com>
2018-05-14 12:44:01 +0000

    add BooksController.php

---
+++ cms/app/Http/Controllers/BooksController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book; //Bookモデルを使えるようにする
+use Validator; //バリデーションを使えるようにする
+
+
+class BooksController extends Controller
+{
+    //更新
+    public function update(Request $request) {
+            $validator = Validator::make($request->all(), [
+            'id' => 'required',
+            'item_name' => 'required|min:3|max:255',
+            'item_number' => 'required|min:1|max:3',
+            'item_amount' => 'required|max:6',
+            'published' => 'required',
+    ]);
+    //バリデーション:エラー
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+    }
+    //データ更新
+    $books = Book::find($request->id);
+    $books->item_name   = $request->item_name;
+    $books->item_number = $request->item_number;
+    $books->item_amount = $request->item_amount;
+    $books->published   = $request->published;
+    $books->save();
+    return redirect('/');
+    }
+}
